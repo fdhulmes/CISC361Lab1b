427ba8e4873ae78201e940c8bcf7a7b9312217bd
Frank <fdhulmes@udel.edu>
2018-09-13 21:41:07 -0400

    Fully working cp function

--- mycp.c
+++ mycp.c
@@ -1,3 +1,8 @@
+/* Frank Hulmes
+  Project 1b
+  CISC361
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +21,7 @@ int main(int argc, char *argv[]){
     printf("Insufficient arguments. Exiting cp ...\n");
     return 0;
   }
+  // Check if both files exist
   int accessRead = access(argv[1], F_OK);
   int accessWrite = access(argv[2], F_OK);
   if((accessRead == 0) && (accessWrite == 0)){
@@ -30,7 +36,7 @@ int main(int argc, char *argv[]){
     }
   }
   else if(accessRead == 0){
-    // To Do: Read the file, create and write to new file
+    // Read from first file, create and write to second
     readAndWrite(argv[1], argv[2]);
     return 0;
   }
@@ -42,8 +48,10 @@ int main(int argc, char *argv[]){
 }
 
 int checkOverwrite(char *fd){
+  // Checks if the user wants to overwrite file fd
   char *overwrite = (char*)malloc(sizeof(char));
   printf("Do you wish to overwrite the file %s ? (y/n)", fd);
+  // Collect and check user input
   scanf("%s", overwrite);
   if(strcmp(overwrite,"y") == 0){
     free(overwrite);
@@ -58,7 +66,9 @@ int checkOverwrite(char *fd){
 int readAndWrite(char *readfd, char *writefd){
   // Opens, reads, and writes to the file
   int openRead, openWrite, readIn, writeIn;
+  // Buffer to be read into
   char *buffer = (char *)malloc(sizeof(char) * MAX_SIZE);
+  // Open the read and write files, checking for errors
   openRead = open(readfd, O_RDONLY);
   if(openRead < 0){
     perror("ERROR: ");
@@ -69,8 +79,9 @@ int readAndWrite(char *readfd, char *writefd){
     perror("ERROR: ");
     return 0;
   }
+  // Read in the first char, check for errors, then write the first char
   readIn = read(openRead, buffer, MAX_SIZE);
-  if(read < 0){
+  if(readIn < 0){
     perror("ERROR: ");
     return 0;
   }
@@ -80,6 +91,7 @@ int readAndWrite(char *readfd, char *writefd){
       perror("ERROR: ");
       return 0;
     }
+    // Keep looping until end of read file
     else{
       while(read(openRead, buffer, MAX_SIZE) > 0){
         writeIn = write(openWrite, buffer, MAX_SIZE);
--- mycph.h
+++ mycph.h
@@ -1,3 +1,25 @@
+/* Frank Hulmes
+  Project 1b
+  CISC361
+                                    mycp Header
+
+  checkOverwrite(char *fd) - asks the user if they wish to overwrite the files
+    parameters:
+      char *fd - string containing the filename
+    returns:
+      1 if it can overwrite, 0 if Not
+*/
+
 int checkOverwrite(char *fd);
+
+/*
+  readAndWrite(char *readfd, char *writefd) - reads from filename readfd and writes to
+  filename writefd, either overwriting the current file or creating a new one
+    parameters:
+      char *readfd - string containing the filename to be read
+      char *writefd - string containing the filename to be written to
+    returns:
+      1 if successful, 0 if Not
+*/
 
 int readAndWrite(char *readfd, char *writefd);
